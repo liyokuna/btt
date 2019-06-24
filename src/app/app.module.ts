7360c2bfd756d11ffd2ac2f0ecc11760bd0c2ebd
liyokuna <liyokuna@yahoo.fr>
2019-06-25 01:30:24 +0200

    center the home html text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { TwPageComponent } from './twitter-page/tw-page.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 
 @NgModule({
   declarations: [
@@ -14,6 +24,14 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
     NgbModule
   ],
   providers: [],

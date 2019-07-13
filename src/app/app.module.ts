9d6df3fcb131f49aee8453cd9b7d3f869787d4fe
liyokuna <liyokuna@yahoo.fr>
2019-07-13 17:51:40 +0200

    adding ReactiveForms inside module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
-import { TwPageComponent } from './twitter-page/tw-page.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TwPageComponent } from './twitter-page/tw-page.component';
 import { TwitterService } from './services/twitter-service.service';
 
 // AoT requires an exported function for factories
@@ -28,6 +29,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     TranslateModule.forRoot({
       loader: {
           provide: TranslateLoader,

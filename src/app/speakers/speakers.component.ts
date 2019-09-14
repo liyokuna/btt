63b422c6789c7cfbac4b567086511e3c6c323327
liyokuna <liyokuna@yahoo.fr>
2019-09-14 16:56:38 +0200

    add(home):adding glitch

--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -12,7 +12,7 @@ export class SpeakersComponent implements OnInit {
 
   ngOnInit() {
     this.ttservice.getSpeakers().subscribe((res) => {
-      console.log('launched');
+      console.log(res);
     });
   }
 

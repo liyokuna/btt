39f8cbba96b635c3c174096379047194ecdefa50
liyokuna <liyokuna@yahoo.fr>
2019-10-25 19:57:02 +0200

    importing cookie manager service, adding translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TwitterService } from './services/twitter-service.service';
+import { CookiemanagerService } from './services/cookiemanager.service';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +12,30 @@ import { TwitterService } from './services/twitter-service.service';
 export class AppComponent implements OnInit {
   title = 'Btt';
   click: boolean;
-  constructor(translate: TranslateService, private http: HttpClient, private ttservice: TwitterService) {
+  langSaved;
+  constructor(private translate: TranslateService, private http: HttpClient,
+     private ttservice: TwitterService, private cookiemanager: CookiemanagerService) {
     translate.setDefaultLang('en');
-    translate.use('en');
     this.click = false;
   }
 
   ngOnInit() {
-    this.ttservice.getRepo().subscribe((res)=>{
+    this.langSaved = this.cookiemanager.getCookie('lang');
+    this.ttservice.getRepo().subscribe((res)=> {
       console.info('repo available');
-    })
+    });
+    if (this.langSaved) {
+      this.translate.use(this.langSaved);
+    }
   }
 
   ToggleClass() {
     this.click = ! this.click;
+  }
+
+  setLanguage(language: string) {
+    this.translate.use(language);
+    this.cookiemanager.setCookieWithString('lang', language, 'localhost');
+    this.langSaved = language;
   }
 }

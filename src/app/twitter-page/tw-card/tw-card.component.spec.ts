4f756b82ec01290e53914911cb2b15f1a5dec617
liyokuna <liyokuna@yahoo.fr>
2020-04-19 18:45:49 +0200

    imroving test

--- src/app/twitter-page/tw-card/tw-card.component.spec.ts
+++ src/app/twitter-page/tw-card/tw-card.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
 import { TwCardComponent } from './tw-card.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 describe('TwCardComponent', () => {
   let component: TwCardComponent;
@@ -8,6 +15,16 @@ describe('TwCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+              provide: TranslateLoader,
+              useFactory: HttpLoaderFactory,
+              deps: [HttpClient]
+          }
+        })
+      ],
       declarations: [ TwCardComponent ]
     })
     .compileComponents();
@@ -16,6 +33,14 @@ describe('TwCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TwCardComponent);
     component = fixture.componentInstance;
+    component.item = {
+      'text':'text',
+      'user': {
+        'name':'name',
+        'screen_name':'screen_name',
+        'profile_image_url':'home'
+      },
+    };
     fixture.detectChanges();
   });
 

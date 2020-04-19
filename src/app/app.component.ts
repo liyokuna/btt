d8b2593ecfa482b36bdac09aef3b7617835c1879
liyokuna <liyokuna@yahoo.fr>
2020-04-19 17:31:10 +0200

    redefining the varaible name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
@@ -11,18 +11,17 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Btt';
   domain = 'https://btt.netlify.com/';
   click: boolean;
   langSaved;
-  path_logo: string;
+  pathLogo: string;
   ttserviceSubscription: Subscription;
-  routingSvg = ['speakers','tweets'];
   constructor(private translate: TranslateService, private http: HttpClient,
               private ttservice: TwitterService, private cookiemanager: CookiemanagerService,
-              private router:  Router) {
-    router.events.subscribe( (event) => ( event instanceof NavigationEnd ) && this.handleRouteChange() )
+              private router: Router) {
+    router.events.subscribe( (event) => ( event instanceof NavigationEnd ) && this.handleRouteChange());
     translate.setDefaultLang('en');
     this.click = false;
   }
@@ -30,7 +29,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.langSaved = this.cookiemanager.getCookie('lang');
     this.ttserviceSubscription = this.ttservice.getRepo().subscribe((res) => {
-      console.log('repo available'+ res);
+      console.log('repo available' + res);
     });
     if (this.langSaved) {
       this.translate.use(this.langSaved);
@@ -49,14 +48,14 @@ export class AppComponent implements OnInit {
 
   handleRouteChange() {
     if (this.router.url.includes('speakers')) {
-      this.path_logo = 'speaker';
+      this.pathLogo = 'speaker';
       return;
     }
     if (this.router.url.includes('tweets')) {
-      this.path_logo = 'tweet';
+      this.pathLogo = 'tweet';
       return;
     }
-    this.path_logo = 'home';
+    this.pathLogo = 'home';
     return;
   }
 
